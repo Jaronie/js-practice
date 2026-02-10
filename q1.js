bcf0d37a107332ca0856ba93780c969fd29f30a2
jaronie <[200~Jisaa@BALONEY MINGW64 ~$ git config --global user.name jaronieJisaa@BALONEY MINGW64 ~$ git config --global user.email>
2026-02-10 03:56:05 -0800

    q1 finished

--- q1.js
+++ q1.js
@@ -1,10 +1,18 @@
 // 1. Calculate the sum of all numbers in an array (using a loop)
 
 function sumArray(array) {
+    let sum = 0; //variavble to store sum
+    for (let i = 0; i < array.length; i++){
+        sum += array[i]; // increment sum by each element in the array
+
+    }
+
+    console.log(sum); // print sum to console
 
 }
 
 // Test for sum array
 let testArray = [1, 2, 3, 4, 5]; // 15
+sumArray(testArray);
 
 // TODO: display the sum of the array in the console
