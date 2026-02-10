e108f8e3a73a015b60afed81e0eac73accd89628
jaronie <[200~Jisaa@BALONEY MINGW64 ~$ git config --global user.name jaronieJisaa@BALONEY MINGW64 ~$ git config --global user.email>
2026-02-10 04:07:15 -0800

    q4 finished.

--- q4.js
+++ q4.js
@@ -3,12 +3,23 @@ A palindrome is a word, number, or other sequence of characters that reads the s
 */
 
 function checkPalindrome(textInput) {
-    
+    let x = "";
+    for(let i = textInput.length - 1; i >= 0; i--){ // loop through text input backwards
+        x += textInput[i]; // add each character to x
+    }
+    if(x === textInput){
+        console.log(textInput + " is a palindrone: " + true); // if x is the same as text input, it's a palindrome
+    } else {
+        console.log(textInput + " is a palindrone: " + false);
+    }
 }
 
 // Test for palindrome
 
 let testString = "racecar"; // true
 let testString2 = "hello"; // false
+checkPalindrome(testString);
+checkPalindrome(testString2);
+
 
 // TODO: display the result of the test strings in the console
