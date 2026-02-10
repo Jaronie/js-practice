cc286bbb0dfe1d8dfb3f67a1df64d730fd47b234
jaronie <[200~Jisaa@BALONEY MINGW64 ~$ git config --global user.name jaronieJisaa@BALONEY MINGW64 ~$ git config --global user.email>
2026-02-10 04:07:04 -0800

    q3 finished.

--- q3.js
+++ q3.js
@@ -1,5 +1,13 @@
 // 3. Remove duplicates from an array (using a loop). FYI: array.includes(element) checks if an element is in an array
 function removeDuplicates(array) {
+    let new_array = []; // initialize empty array
+    for(let i = 0; i < array.length; i++){ // loop through array
+        if(!new_array.includes(array[i])){ // if new array doesn't include current i, add it
+            // avoids adding duplicates
+            new_array.push(array[i]);
+        }}
+
+    console.log(new_array); // print new array to console
 
 }
 
