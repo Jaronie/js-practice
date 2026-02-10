2284d30ce4d39a695a71534d96f6790ede8c4b98
jaronie <[200~Jisaa@BALONEY MINGW64 ~$ git config --global user.name jaronieJisaa@BALONEY MINGW64 ~$ git config --global user.email>
2026-02-10 04:06:54 -0800

    q2 finished.

--- q2.js
+++ q2.js
@@ -1,9 +1,19 @@
 // 2. Find the largest number in an array (using a loop)
 function findLargest(array) {
+    largest = array[0]; // storing largest number in the array in a variable, starting at the 0 index
+
+    for(let i = 0; i < array.length; i++){ //loop through array
+        if(array[i] > largest) // if i is greater than largest, reassign largest to i
+        {
+            largest = array[i];
+            }    
+
+            }
+    console.log(largest); // print largest to console
 
 }
 
 // Test for find largest
 let testArray2 = [1, 4, 5, 4, 3]; // 5
-
+findLargest(testArray2);
 // TODO: display the largest number in the array in the console
